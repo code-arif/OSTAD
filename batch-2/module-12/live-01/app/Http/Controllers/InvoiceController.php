2eb2f6bfd0abccd40d412d331300570b3eec016c
code-arif <ahsanullahariful6460@gmail.com>
2024-05-27 09:24:52 +0600

    live class-01 modifed

--- batch-2/module-12/live-01/app/Http/Controllers/InvoiceController.php
+++ batch-2/module-12/live-01/app/Http/Controllers/InvoiceController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 class InvoiceController extends Controller
 {
     public function getData(){
-        $invoice = Invoice::get();
+        $invoice = Invoice::limit(5)->get();
         return $invoice;
+    }
+
+    public function invoice($id){
+        $invoice = Invoice::findOrFail($id);
+        return $invoice->client;
     }
 }
--- batch-2/module-12/live-01/routes/web.php
+++ batch-2/module-12/live-01/routes/web.php
@@ -8,3 +8,4 @@ Route::get('/', function () {
 });
 
 Route::get('data',[InvoiceController::class,'getData']);
+Route::get('/invoice/{id}',[InvoiceController::class,'invoice']);
