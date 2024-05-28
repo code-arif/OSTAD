43ee1f1e5e595b1a431c4bb3277abcb42ee20e99
code-arif <arifulislam6460@gmail.com>
2024-05-28 15:33:55 +0600

    module-12 live-01 edited

--- batch-2/module-12/live-01/app/Http/Controllers/InvoiceController.php
+++ batch-2/module-12/live-01/app/Http/Controllers/InvoiceController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class InvoiceController extends Controller
 {
     public function getData(){
-        $invoice = Invoice::limit(5)->get();
+        // $invoice = Invoice::limit(5)->get();
+        // $invoice = Invoice::where('paid','=',1)->get();
+        // $invoice = Invoice::whereTotalPrice(850000)->get();
+        // $invoice = Invoice::where('paid',1,)->where('total_price','<',80000)->get();
+        $invoice = Invoice::where([
+        ['paid', 1],
+        ['total_price', '>', 80000] ])->get();
         return $invoice;
     }
 
