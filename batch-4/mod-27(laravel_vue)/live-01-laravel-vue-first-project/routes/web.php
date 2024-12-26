dd121d18d2d0f2deee96bb73bc126de54528d4d9
code-arif <ahsanullahariful6460@gmail.com>
2024-12-26 22:48:01 +0600

    marge

--- batch-4/mod-27(laravel_vue)/live-01-laravel-vue-first-project/routes/web.php
+++ batch-4/mod-27(laravel_vue)/live-01-laravel-vue-first-project/routes/web.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/',[DemoController::class,'home'])->name('home.show');
 Route::get('/profile',[DemoController::class,'profile'])->name('profile.show');
